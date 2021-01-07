52733b8d425be5f376d60b36131739d18ae1489a
Edward Chao <edward_888@hotmail.com>
2021-01-07 17:09:25 -0500

    inital vulkan tutorial files

---
+++ VulkanTutorial/VulkanTutorial/main.cpp
@@ -0,0 +1,37 @@
+#include <vulkan/vulkan.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+class HelloTriangleApplication{
+public:
+  void run() {
+    initVulkan();
+    mainLoop();
+    cleanUp();
+  }
+private:
+  void initVulkan(){
+  }
+  void mainLoop() {
+
+  }
+  void cleanUp() {
+
+  }
+};
+
+int main() {
+  HelloTriangleApplication app;
+
+  try{
+    app.run();
+  }
+  catch (const std::exception &e){
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
